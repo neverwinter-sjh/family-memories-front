61aa35da7e4110423a51220956c8c51c090ecfd3
neverwinter-sjh <neverwinter.sjh@gmail.com>
2021-07-24 12:11:24 +0900

    update

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 };
